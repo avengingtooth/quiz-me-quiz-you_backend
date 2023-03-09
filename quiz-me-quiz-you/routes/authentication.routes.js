97a6235b6011e8600aaabe7caef04cfad84d9b98
Mershouta <jchristkay@gmail.com>
2023-03-09 15:31:20 +0100

    Backend auth routes done, unverified

--- quiz-me-quiz-you/routes/authentication.routes.js
+++ quiz-me-quiz-you/routes/authentication.routes.js
@@ -1,15 +1,134 @@
+const express = require("express")
 const router = require("express").Router();
 
-router.post("/login", (req, res, next) => {
+const bcrypt = require("bcrypt");
+const mongoose = require("mongoose");
+
+const User = require("../models/User.model");
+
+const isLoggedOut = require("../middleware/isLoggedOut");
+const isLoggedIn = require("../middleware/isLoggedIn");
+
+
+router.get("/login", isLoggedOut, (req, res) => {
     res.json("login");
 });
+router.post("/login", (req, res, next) => {
+    const { username, email, password } = req.body;
 
-router.post("/signup", (req, res, next) => {
-    res.json("signup");
+    // Check that username, email, and password are provided
+    if (username === "" || email === "" || password === "") {
+        res.status(400).json("login", {
+            errorMessage:
+                "All fields are mandatory. Please provide username, email and password.",
+        });
+
+        return;
+    }
+
+    // - either length based parameters or we check the strength of a password
+    if (password.length < 6) {
+        return res.status(400).json("login", {
+            errorMessage: "Your password needs to be at least 6 characters long.",
+        });
+    }
+
+    // Search the database for a user with the email submitted in the form
+    User.findOne({ email })
+        .then((user) => {
+            // If the user isn't found, send an error message that user provided wrong credentials
+            if (!user) {
+                res
+                    .status(400)
+                    .json("login", { errorMessage: "Wrong credentials." });
+                return;
+            }
+
+            // If user is found based on the username, check if the in putted password matches the one saved in the database
+            bcrypt
+                .compare(password, user.password)
+                .then((isSamePassword) => {
+                    if (!isSamePassword) {
+                        res
+                            .status(400)
+                            .json("login", { errorMessage: "Wrong credentials." });
+                        return;
+                    }
+
+                    // Add the user object to the session object
+                    req.session.currentUser = user.toObject();
+                    // Remove the password field
+                    delete req.session.currentUser.password;
+
+                    res.redirect("/quiz/all");
+                })
+                .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
+        })
+        .catch((err) => next(err));
 });
 
-router.post("/logout", (req, res, next) => {
+
+
+router.get("/signup", isLoggedOut, (req, res) => {
     res.json("signup");
+});
+router.post("/signup", isLoggedOut, (req, res, next) => {
+    const { username, email, password } = req.body;
+
+    // Check that username, email, and password are provided
+    if (username === "" || email === "" || password === "") {
+        res.status(400).json("signup", {
+            errorMessage:
+                "All fields are mandatory. Please provide your username, email and password.",
+        });
+
+        return;
+    }
+
+    if (password.length < 6) {
+        res.status(400).json("signup", {
+            errorMessage: "Your password needs to be at least 6 characters long.",
+        });
+
+        return;
+    }
+
+    bcrypt
+        .genSalt(saltRounds)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hashedPassword) => {
+
+            // Create a user and save it in the database
+            return User.create({ username, email, password: hashedPassword });
+        })
+        .then((user) => {
+            res.redirect("/login");
+        })
+        .catch((error) => {
+            if (error instanceof mongoose.Error.ValidationError) {
+                res.status(500).json("signup", { errorMessage: error.message });
+            } else if (error.code === 11000) {
+                res.status(500).json("signup", {
+                    errorMessage:
+                        "Username and email need to be unique. Provide a valid username or email.",
+                });
+            } else {
+                next(error);
+            }
+        });
+});
+
+
+
+router.post("/logout", (req, res, next) => {
+    req.session.destroy((err) => {
+        if (err) {
+            res.status(500).json("logout", { errorMessage: err.message });
+            return;
+        }
+
+        res.redirect("/");
+    });
 });
 
 
