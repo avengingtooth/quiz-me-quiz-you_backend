7f4b71587576e55e3736191f1f0c5ef920c71f14
avengingtooth <esellin@gmail.com>
2023-03-09 17:58:56 +0100

    seeding pushes to db

--- quiz-me-quiz-you/seeds/seed.js
+++ quiz-me-quiz-you/seeds/seed.js
@@ -1,60 +1,86 @@
-const mongoose = require('mongoose')
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/0-seeding-and-ajax'
-
-const users = require('./user.json')
-const quizzes = require('./quiz.json')
-const questions = require('./question.json')
+const mongoose = require("../db");
 
 const User = require('../models/User.model')
 const Quiz = require('../models/Quiz-model')
 const Question = require('../models/Question-model')
 
-mongoose
-    .set('strictQuery', false)
-    .connect(MONGO_URI)
-    .then(async (x) => {
-        try {
-            const dbName = x.connections[0].name
-            await seedUsers()
-            await seedQuizzes()
-            await seedQuestions()
+async function seed(){
+    const users = require('./user.json')
+    const quizzes = require('./quiz.json')
+    const questions = require('./question.json')
+    const userObjIds = {}
+    await Promise.all([
+        User.deleteMany(),
+        Quiz.deleteMany(),
+        Question.deleteMany(),
+    ])
 
-            await mongoose.disconnect()
-        } catch (error) {
-            console.error(error)
+    for(let i = 0; i < users.length; i++){
+        userObjIds[users[i].username] = await User.create(users[i])
+    }
+
+    for(let i = 0; i < quizzes.length; i++){
+        let quiz = quizzes[i]
+        let questionObjIds = []
+        for(let i = 0; i < quiz.questions.length; i++){
+            questionObjIds.push(await Question.create(quiz.questions[i]))
         }
-    })
-    .catch((err) => {
-        console.error('Error connecting to mondo: ', err)
-    })
-
-async function seedUsers() {
-    try {
-        await User.deleteMany()
-        await User.create(users)
-    } catch (error) {
-        console.log(error);
+        quiz.owner = userObjIds[quiz.owner]
+        quiz.questions = questionObjIds
+        await Quiz.create(quiz)
     }
+
+    mongoose.connection.close();
 }
 
-async function seedQuizzes() {
-    try {
-        await Quiz.deleteMany()
-        for (const quiz of quizzes) {
-            const user = await User.findOne({ username: quiz.owner })
-            quiz.owner = user._id;
-        }
-        await Quiz.create(quizzes)
-    } catch (error) {
-        console.log(error);
-    }
-}
+// mongoose
+//     .set('strictQuery', false)
+//     .connect(MONGO_URI)
+//     .then(async (x) => {
+//         try {
+//             const dbName = x.connections[0].name
+//             await seedUsers()
+//             await seedQuizzes()
+//             await seedQuestions()
 
-async function seedQuestions() {
-    try {
-        await Question.deleteMany()
-        await Question.create(questions)
-    } catch (error) {
-        console.log(error);
-    }
-}
+//             await mongoose.disconnect()
+//         } catch (error) {
+//             console.error(error)
+//         }
+//     })
+//     .catch((err) => {
+//         console.error('Error connecting to mondo: ', err)
+//     })
+
+// async function seedUsers() {
+//     try {
+//         await User.deleteMany()
+//         await User.create(users)
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// async function seedQuizzes() {
+//     try {
+//         await Quiz.deleteMany()
+//         for (const quiz of quizzes) {
+//             const user = await User.findOne({ username: quiz.owner })
+//             quiz.owner = user._id;
+//         }
+//         await Quiz.create(quizzes)
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// async function seedQuestions() {
+//     try {
+//         await Question.deleteMany()
+//         await Question.create(questions)
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+seed()
