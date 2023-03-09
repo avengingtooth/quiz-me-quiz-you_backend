d2cadcf0255052a57b3e5cfd8c57030c9117e950
TheMike1972 <TheMike1972@github.com>
2023-03-09 09:38:32 +0100

    begin seeds

---
+++ quiz-me-quiz-you/seeds/seed.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const MONGO_URI =
+
+const users = require('./user.json')
+const quizzes = require('./quiz.json')
+const questions = require('./question.json')
+
+const User = require('../models/User.model')
+const Quiz = require('../models/')
