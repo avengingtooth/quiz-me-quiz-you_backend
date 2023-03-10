1742d12443efdd92f1c79729283661dcf67b6129
TheMike1972 <TheMike1972@github.com>
2023-03-10 12:36:42 +0100

    small modifications in models

--- quiz-me-quiz-you/seeds/seed.js
+++ quiz-me-quiz-you/seeds/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/0-seeding-and-ajax'
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/quiz-me-quiz-you'
 
 const users = require('./user.json')
 const quizzes = require('./quiz.json')
