1018a3772fe876a0f2dc203ae98932b5603c6e89
avengingtooth <esellin@gmail.com>
2023-03-17 15:11:19 +0100

    patch

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const express = require("express");
 
 const app = express();
 
+const cors = require('cors')
+
+app.use(cors)
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
